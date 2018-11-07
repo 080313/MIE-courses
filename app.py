8e63a9344055559bc8545ee12d67bfa07dee069e
fnavab <31870240+fnavab@users.noreply.github.com>
2018-11-07 11:36:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import todo_db
 
 
 # Define a path for the database
-DATABASE_PATH = "db.db"
+DATABASE_PATH = "data.db"
 
 # create a Flask app
 app = Flask(__name__)
 
 def get_db_conn():
 	if not hasattr(g, "_db_conn"):
-		g._db_conn = todo_db.initialize_db("db.db")
+		g._db_conn = todo_db.initialize_db("data.db")
 	return g._db_conn
 
 # Welcome page. Accessible at <server-address>/
@@ -20,26 +20,50 @@ def welcome_page():
     return render_template('main.html')
 
 # Todo list page. Accessible at <server-address>/todo
-@app.route('/todo')
+@app.route('/IndyCourses')
 def todo_list():
-	db_conn = get_db_conn()
-	task_list = todo_db.get_tasks(db_conn)
-	return render_template('todolist.html', task_list=task_list)
+	#db_conn = get_db_conn()
+	#task_list = todo_db.get_tasks(db_conn)
+	return render_template('indyCourses.html')
 
 @app.route('/thirdYearCourse')
 def thirsYearCourse():
-	db_conn = get_db_conn()
-	task_list = todo_db.get_tasks(db_conn)
-	return render_template('thirdYearCourse.html', task_list=task_list)
+	#db_conn = get_db_conn()
+	#task_list = todo_db.get_forms(db_conn)
+	return render_template('thirdYearCourse.html')
+
+@app.route('/form')
+def form():
+	#db_conn = get_db_conn()
+	#task_list = todo_db.get_tasks(db_conn)
+	return render_template('form.html')
 
 # Handles add task request. The task details are submitted by a HTML form with an action="/add".
 # This function extract the form field "title" and callls the app function add_task
 @app.route('/add', methods=['GET'])
-def add_task():
-	task_title = request.args.get('title')
+def add_form():
+	CourseCode = request.args.get('CourseCode')
+	NickName = request.args.get('NickName')
+	ProfName = request.args.get('ProfName')
+	Session = request.args.get('Session')
+	TAName = request.args.get('TA Name')
+	LabTut = request.args.get('Lab/Tut')
+	FinalGrade = request.args.get('Final Grade')
+	LoD = request.args.get('Level of Difficulty')
+	workload = request.args.get('workload')
+	interesting = request.args.get('interesting')
+	OverallRating = request.args.get('Overall Rating')
 	db_conn = get_db_conn()
-	task_list = todo_db.add_task(db_conn, task_title)
-	return redirect("/todo", code=307)
+	todo_db.add_forms(db_conn,CourseCode,NickName, ProfName,Session,TAName,LabTut,FinalGrade,LoD,workload,interesting,OverallRating)
+	return redirect("/reviews", code=307)
+
+@app.route('/reviews')
+def display():
+	db_conn = get_db_conn()
+	review_list = todo_db.get_forms(db_conn)
+	
+	return  render_template('reviews.html', review_list=review_list)
+
 
 # Handles requests to mark task as done.
 # The task id of the task to be marked as done is passed using the URL, e.g., /set_done/123 is a request to mark task 123 as done.
@@ -68,5 +92,8 @@ def remove_task(task_id):
 	todo_db.remove_task(db_conn, task_id)
 	return redirect("/todo", code=307)
 
+
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
+	
+	
--- todo_db.py
+++ todo_db.py
@@ -1,59 +1,196 @@
 import pyodbc
 import sqlite3
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=O:\navabzad\Documents\Drama.accdb;')
-cursor = conn.cursor()
-param="Alice"
-cursor.execute("SELECT gender FROM students WHERE student= ?",param);
+#conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=O:\navabzad\Documents\Drama.accdb;')
+#cursor = conn.cursor()
+#param="Alice"
+#cursor.execute("SELECT gender FROM students WHERE student= ?",param);
 
-for row in cursor.fetchall():
-	print(row)
+#for row in cursor.fetchall():
+	#print(row)
         
 
 
-class Task(object):
-	def __init__(self, task_id, task, done):
-		self.task_id = task_id
-		self.task = task
-		self.done = done
 
+#class Student(Object):
+	#def _init_(self,)
+
+class Form1(object):
+	def __init__(self,CourseCode,NickName,ProfName,Session,TAName,LABTUT, FinalGrade,LevelDifficulty,WorkLoad,Interesting,OverallRating):
+		self.CourseCode=CourseCode
+		self.NickName=NickName
+		self.ProfName=ProfName
+		self.Session=Session
+		self.TAName=TAName
+		self.LABTUT=LABTUT
+		self.FinalGrade=FinalGrade
+		self.LevelDifficulty=LevelDifficulty
+		self.WorkLoad=WorkLoad
+		self.Interesting=Interesting
+		self.OverallRating=OverallRating
+class Course(object):
+	def __init__(self,CourseName,CorseCode,Session):
+		self.CourseName=CourseName
+		self.CourseCode=CorseCode
+		self.Session=Session
+		
+		
+class TA(object):
+	def __init__(self,TAName,TUTLAB,CorseCode):
+		self.TAName = TAName
+		self.TUTLAB=CorseCode
+
+class Student(object):
+	def __init__(self,NickName,INDYMECH,Password,Email ):
+		self.NickName=NickName
+		self.INDYMECH=INDYMECH	
+		self.Password=Password
+		self.email=Email
+
+class Prof(object):
+	def __init__(self,ProfName,INDYMECH):
+		self.ProfName=ProfName
+		self.INDYMECH=INDYMECH
+	
 
 def initialize_db(db_path):
 	conn = sqlite3.connect(db_path)
-	conn.execute(INITIALIZE_DB)
-	conn.commit()
+	#conn.execute(INITIALIZE_DB)
+	#conn.commit()
 	return conn
 
 
 def get_tasks(db_conn):
-	results = db_conn.execute("SELECT task_id, task, done FROM todo;")
+	results = db_conn.execute("SELECT TAName FROM TA")
 
 	task_list = []
-	for task_id, task, done in results:
-		current_task = Task(task_id, task, done)
+	for taName in results:
+		print(taName)
+		current_task = TA(taName)
+		
 		task_list.append(current_task)
 
 	return task_list
 
-def add_task(db_conn, task):
-	if not task:
-		return
-	sqlInsert = "INSERT INTO todo (task, done) VALUES ('{}', '{})".format(task)
+
+def add_forms(db_conn,CourseCode, NickName, ProfName, Session,TAName, LABTUT, FinalGrade, LevelDifficulty, WorkLoad, Interesting, OverallRating ):
+	#if not recordC:
+	#	return
+	
+	sqlInsert = "INSERT INTO Form (CourseCode, NickName, ProfName, Session,TAName, LABTUT, FinalGrade, LevelDifficulty, WorkLoad, Interesting, OverallRating) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(CourseCode, NickName, ProfName, Session,TAName, LABTUT, FinalGrade, LevelDifficulty, WorkLoad, Interesting, OverallRating)
 	db_conn.execute(sqlInsert)
 	db_conn.commit()
 
-def set_done(db_conn, task_id):
-	sqlUpdate = "UPDATE todo SET done=1 WHERE task_id={}".format(task_id,)
-	db_conn.execute(sqlUpdate)
+
+def get_forms(db_conn):
+	results = db_conn.execute("SELECT * FROM Form")
+	data=results.fetchall()
+	#record_list = []
+	#for CourseCode, NickName, ProfName, Session,TAName, LABTUT, FinalGrade, LevelDifficulty, WorkLoad, Interesting, OverallRating in results:
+		#print(NickName)
+	#	current_record = Form1(CourseCode, NickName, ProfName, Session,TAName, LABTUT, FinalGrade, LevelDifficulty, WorkLoad, Interesting, OverallRating)
+		
+	#	record_list.append(current_record)
+	return data
+	#return record_list
+def add_course(db_conn,CourseName,CorseCode,Session):
+	sqlInsert = "INSERT INTO Course (CourseName,CorseCode,Session) VALUES ('{}','{}','{}')".format(CourseName,CorseCode,Session)
+	db_conn.execute(sqlInsert)
+	db_conn.commit()
+	
+
+def get_Course(db_conn):
+	results = db_conn.execute("SELECT * FROM Course")
+
+	course_list = []
+	for CourseName,CourseCode, Session in results:
+		print(CourseName)
+		current_course =Course(CourseName,CourseCode,Session)
+		
+		course_list.append(current_course)
+
+def add_course(db_conn,CourseName,CorseCode,Session):
+	sqlInsert = "INSERT INTO Course (CourseName,CorseCode,Session) VALUES ('{}','{}','{}')".format(CourseName,CorseCode,Session)
+	db_conn.execute(sqlInsert)
+	db_conn.commit()
+	
+
+def add_TA(db_conn,TAName,TUTLAB, CorseCode):
+	sqlInsert = "INSERT INTO TA (TAName,TUTLAB, CorseCode) VALUES ('{}','{}','{}')".format(TAName,TUTLAB, CorseCode)
+	db_conn.execute(sqlInsert)
 	db_conn.commit()
 
-def set_not_done(db_conn, task_id):
-	sqlUpdate = "UPDATE todo SET done=0 WHERE task_id={}".format(task_id)
-	db_conn.execute(sqlUpdate)
+def get_TA(db_conn):
+	results = db_conn.execute("SELECT * FROM TA")
+
+	ta_list = []
+	for TAName,TUTLAB, CorseCode in results:
+		print(TAName)
+		current_TA=TA(TAName,TUTLAB, CorseCode)
+		
+		ta_list.append(current_TA)
+		
+def add_Student(db_conn,NickName,INDYMECH,Password,Email):
+	sqlInsert = "INSERT INTO Student (NickName,INDYMECH,Password,Email) VALUES ('{}','{}','{}','{}')".format(NickName,INDYMECH,Password,Email)
+	db_conn.execute(sqlInsert)
 	db_conn.commit()
 
-def remove_task(db_conn, task_id):
-	sqlDelete = "DELETE FROM todo WHERE task_id={}".format(task_id)
-	db_conn.execute(sqlDelete)
+def get_Student(db_conn):
+	results = db_conn.execute("SELECT * FROM Student")
+
+	student_list = []
+	for NickName,INDYMECH,Password,Email in results:
+		print(NickName)
+		current_Student=Student(NickName,INDYMECH,Password,Email )
+		
+		student_list.append(current_Student)
+		
+def add_Prof(db_conn,ProfName,INDYMECH):
+	sqlInsert = "INSERT INTO Professor (ProfName,INDYMECH) VALUES ('{}','{}')".format(ProfName,INDYMECH)
+	db_conn.execute(sqlInsert)
 	db_conn.commit()
 
+def get_Prof(db_conn):
+	results = db_conn.execute("SELECT * FROM Professor")
+
+	prof_list = []
+	for ProfName,INDYMECH in results:
+		print(ProfName)
+		current_Prof=Prof(ProfName,INDYMECH)
+		
+		prof_list.append(current_Prof)
+
+
+
+#def set_done(db_conn, task_id):
+#	sqlUpdate = "UPDATE todo SET done=1 WHERE task_id={}".format(task_id,)
+#	db_conn.execute(sqlUpdate)
+#	db_conn.commit()
+
+#def set_not_done(db_conn, task_id):
+#	sqlUpdate = "UPDATE todo SET done=0 WHERE task_id={}".format(task_id)
+#	db_conn.execute(sqlUpdate)
+#	db_conn.commit()
+
+#def remove_task(db_conn, task_id):
+#	sqlDelete = "DELETE FROM todo WHERE task_id={}".format(task_id)
+#	db_conn.execute(sqlDelete)
+#	db_conn.commit()
+
+
+
+
+
+
+
+#conn=initialize_db("data.sqlite")
+#get_tasks(conn)
+
+
+# add_form(conn,"MIE263","alice","Neal Kaw","W","Bob Zedd","0102",63,5,4,3,4)
+#get_forms(conn)
+#add_course(conn,"Simulation","MIE360","W")
+#get_Course(conn)
+#get_TA(conn)
+#get_Student(conn)
+#get_Prof(conn)
